5da6c9613933bc5b1657df0f136b90b74b584d81
icodezjb <icodezjb@users.noreply.github.com>
2024-11-03 17:30:32 +0800

    Add withdrawToLightning with SystemWithdrawV2 lib

---
+++ src/lib/IWithdrawPrecompile.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+interface IWithdrawPrecompile {
+    function withdrawBitcoinAssets(address, uint256, string memory) external;
+    function withdrawGovToken(uint256, bytes32) external;
+    function queryGovToken() external view returns (address);
+    function queryCurrentFees() external view returns (uint64, uint64, uint64);
+    function withdrawLightning(address, uint256) external;
+}
