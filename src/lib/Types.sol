aa4423c9ad1410bd5eff0eb5138e170e225a8d46
icodezjb <icodezjb@users.noreply.github.com>
2025-12-09 23:46:41 +0800

    Add account management fee and upgrade to BridgeV8

--- src/lib/Types.sol
+++ src/lib/Types.sol
@@ -64,3 +64,13 @@ struct InitialParameters {
     uint64 wasmDepositNonce; // 8 bytes
     string gasType; // string
 }
+
+// BridgeV7 - Account Management Fee
+struct AccountManagementFee {
+    // Account address
+    address account;
+    // Last withdrawal timestamp
+    uint64 lastWithdrawTimestamp;
+    // Total accumulated fee in satoshis
+    uint256 accumulatedFee;
+}
